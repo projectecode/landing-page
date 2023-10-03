2210d57db08397ea57d54c12361f3509a52901ea
Aditya Ekawade <78055556+projectecode@users.noreply.github.com>
2023-10-02 22:53:15 -0700

    install scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,7 @@
 <template>
     <nav class="site-header sticky-top py-1">
       <div class="container d-flex flex-column flex-md-row justify-content-between">
-        <a class="py-2" href="#" aria-label="Product">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="d-block mx-auto"
-            role="img"
-            viewBox="0 0 24 24"
-            focusable="false"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path
-              d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"
-            />
-          </svg>
-        </a>
+        <a class="py-2" href="#">Logo</a>
         <a class="py-2 d-none d-md-inline-block" href="#">Menu</a>
         <a class="py-2 d-none d-md-inline-block" href="#">Order</a>
         <a class="py-2 d-none d-md-inline-block" href="#">About</a>
@@ -29,7 +9,7 @@
         <a class="py-2 d-none d-md-inline-block" href="#">Offers</a>
       </div>
     </nav>
-
+    <div class="test">Hello</div>
     <div class="jumbotron">
       <div class="row">
         <div class="col-md-12" style="text-align: center;">
