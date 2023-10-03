79a3f34ae513520b0364c3e3a65dbb609fd5c90c
Aditya Ekawade <78055556+projectecode@users.noreply.github.com>
2023-10-02 22:10:37 -0700

    Formatting nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css'},
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap' }
-      ],
+      ]
     }
   }
 })
