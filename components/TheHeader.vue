ad41d1920d0321f825c3d499626d79afc7f14ba0
Aditya Ekawade <78055556+projectecode@users.noreply.github.com>
2023-10-11 21:49:22 -0700

    update header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,46 @@
 <template>
   <nav class="site-header sticky-top py-1">
-    <div class="container d-flex flex-column flex-md-row justify-content-between">
-    <a class="py-2" href="#">Logo</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Menu</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Order</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">About</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Restaurants</a>
-    <a class="py-2 d-none d-md-inline-block" href="#">Offers</a>
+    <div class="container d-flex flex-md-row justify-content-between">
+    <a class="py-2" href="/">
+      <img src="~/assets/logo.svg">
+    </a>
+    <a
+      v-for="item in navItems"
+      :key="item"
+      :href="item.route"
+      :target="item.target"
+      class="py-2 d-none d-md-inline-block"
+    >
+      {{ item.name }}
+    </a>
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  data () {
+    return {
+      navItems: [
+        {name: 'Menu', route: '/menu', target: '_self'},
+        {name: 'About', route: '/about', target: '_self'},
+        {name: 'Restaurants', route: '/restaurants', target: '_self'},
+        {name: 'Offers', route: '/offers', target: '_self'},  
+        {name: 'Order', route: 'http://doordash.com/store/23u82328', target: '_blank'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+nav {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+}
+
+nav a {
+  color: #dd0031 !important;
+  font-weight: 600;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -30,3 +30,41 @@
     </div>
   </div>
 </template>
+
+<style lang="scss">
+body {
+  font-family: "Poppins", sans-serif;
+}
+
+h1 {
+  color: #004f71;
+  font-size: 3rem;
+  line-height: 1.2;
+  font-weight: 900;
+}
+
+.menu-type-btn {
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.25);
+  background-color: white;
+  border-radius: 20px;
+  justify-content: center;
+  margin: 16px 12px;
+  padding: 1.25rem 2rem;
+  font-size: 1.25rem;
+  line-height: 1.5;
+  display: inline-block;
+  text-decoration: none;
+  color: #dd0031;
+  font-weight: 800;
+  word-spacing: 5px;
+}
+.menu-type-btn img {
+  color: #dd0031;
+}
+.menu-type-btn:hover {
+  transform: scale(1.02);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.35);
+  color: #aa0026;
+}
+
+</style>
